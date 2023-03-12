f84d0ff69807481d18713e420bf090c3b02100fc
Gatto lunatico <101564199+PaneSecco@users.noreply.github.com>
2023-03-12 19:15:05 +0100

    Correzione di alcuni problemi e inizio implementazione dell'universalità delle valute

--- Gestione_Colletta/Form1.Designer.cs
+++ Gestione_Colletta/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // listView1
@@ -49,7 +55,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(614, 209);
+            this.button1.Location = new System.Drawing.Point(575, 276);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 32);
             this.button1.TabIndex = 1;
@@ -59,7 +65,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(614, 101);
+            this.textBox1.Location = new System.Drawing.Point(614, 108);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(162, 22);
             this.textBox1.TabIndex = 2;
@@ -74,7 +80,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(525, 107);
+            this.label1.Location = new System.Drawing.Point(525, 114);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 16);
             this.label1.TabIndex = 4;
@@ -89,11 +95,72 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Versamento:";
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(614, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(525, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Valuta:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(697, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 32);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Elimina";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(726, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Totale: 0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(575, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 32);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Cambia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(440, 402);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 483);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -101,7 +168,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Colletta universale";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -116,6 +183,12 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Gestione_Colletta/Form1.cs
+++ Gestione_Colletta/Form1.cs
@@ -12,16 +12,23 @@ namespace Gestione_Colletta
 {
     public partial class Form1 : Form
     {
-        public Dictionary<string, float> colletta;
+        public Dictionary<string, float> versamento;
+        public Dictionary<string, string> valuta;
         public Form1()
         {
             InitializeComponent();
-            colletta = new Dictionary<string, float>();
+            versamento = new Dictionary<string, float>();
+            valuta = new Dictionary<string, string>();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             listView1.Columns.Add("Persona");
             listView1.Columns.Add("Versamento");
+            listView1.Columns.Add("Valuta");
+            comboBox1.Items.Add("euro");
+            comboBox1.Items.Add("sterline");
+            comboBox1.Items.Add("dollari");
+            comboBox1.Items.Add("franchi");
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
@@ -36,38 +43,81 @@ namespace Gestione_Colletta
 
         public void cancella()
         {
-            colletta.Remove(listView1.SelectedItems[0].Text);
-            listview1.SelectedItems[0].Remove();
+            if (listView1.SelectedItems.Count > 0)
+            {
+                versamento.Remove(listView1.SelectedItems[0].Text);
+                valuta.Remove(listView1.SelectedItems[0].Text);
+                listView1.SelectedItems[0].Remove();
+                aggiorna();
+            }
+            else
+            {
+                MessageBox.Show("Scegliere l'elemento da eliminare!");
+                return;
+            }
         }
 
         public void cambia()
         {
-            colletta[textBox1.Text]=textBox2.Text;
-            //da sistemare per la litview
+            versamento[textBox1.Text]= Int32.Parse(textBox2.Text);
+            valuta[textBox1.Text] = comboBox1.SelectedText;
+            aggiorna();
         }
 
         public void aggiungi()
         {
-            if (colletta.ContainsKey(textBox1.Text))
+            if (versamento.ContainsKey(textBox1.Text))
             {
                 MessageBox.Show("Questa persona ha già effettuato un versamento!");
+                return;
             }
             else
             {
-                string[] row = { textBox1.Text, textBox2.Text };
-                var listViewItem = new ListViewItem(row);
-                listView1.Items.Add(listViewItem);
-                colletta.Add(textBox1.Text, float.Parse(textBox2.Text));
+                versamento.Add(textBox1.Text, Int32.Parse(textBox2.Text));
+                valuta.Add(textBox1.Text, comboBox1.SelectedText);
+                aggiorna();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
             }
         }
 
         public void totale()
         {
             float somma=0;
-            for(int i = 0; colletta.Count > i; i++) 
+            for(int i = 0; versamento.Count > i; i++) 
             {
-                somma += colletta.ElementAt(i).Value;
+                somma += versamento.ElementAt(i).Value;
             }
+            label4.Text = "Totale: " + somma;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cancella();
+        }
+
+      
+        public void aggiorna()
+        {
+            listView1.Items.Clear();
+            for(int a=0;a<versamento.Count;a++)
+            {
+                string[] row = { valuta.ElementAt(a).Key, versamento.ElementAt(a).Value.ToString(), valuta.ElementAt(a).Value };
+                var listViewItem = new ListViewItem(row);
+                listView1.Items.Add(listViewItem);
+            }
+            totale();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cambia();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
